02879a1f3cab4f2d7151b30caaf7d60b8cac33b8
sonledevplus <son.le@devplus.edu.vn>
2023-01-21 16:25:05 +0700

    push

--- src/App.js
+++ src/App.js
@@ -69,21 +69,6 @@ function App() {
         </Button>
         {isShow ? (
           <Box position={'fixed'} w='100%' mt='50px'>
-            <Fireworks
-              options={{
-                rocketsPoint: {
-                  min: 0,
-                  max: 100,
-                },
-              }}
-              style={{
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                position: 'fixed',
-              }}
-            />
             <Box position='relative'>
               <Image src='loichuc.png'></Image>
               <Button
@@ -99,6 +84,21 @@ function App() {
                 onClick={handleCloseBtn}>
                 X
               </Button>
+              <Fireworks
+                options={{
+                  rocketsPoint: {
+                    min: 0,
+                    max: 100,
+                  },
+                }}
+                style={{
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  position: 'fixed',
+                }}
+              />
             </Box>
           </Box>
         ) : (
